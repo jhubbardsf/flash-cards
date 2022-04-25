3a282e2327e1ecc92a2c490ed6b2ac94a79cfe7b
Josh Hubbard <jhubbardsf@gmail.com>
2022-04-25 16:06:59 -0400

    Shuffling

--- src/routes/cards.svelte
+++ src/routes/cards.svelte
@@ -15,10 +15,24 @@
 
 <script lang="ts">
 	import Flashcard from '$lib/Flashcard.svelte';
+	import { browser } from '$app/env';
 
 	export let questionAnswers: cardDeck;
 	let currentCard = 0;
 
+	function shuffleArray(questionAnswers: cardDeck) {
+		console.log('Shuffling!');
+		for (let i = questionAnswers.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[questionAnswers[i], questionAnswers[j]] = [questionAnswers[j], questionAnswers[i]];
+		}
+		console.log(questionAnswers);
+
+		questionAnswers = questionAnswers;
+		qA = questionAnswers[0];
+	}
+
+	if (!browser) shuffleArray(questionAnswers);
 	console.log('Flash Game!');
 	console.log({ questionAnswers });
 	$: qA = questionAnswers[currentCard];
@@ -43,13 +57,21 @@
 	<div class="flex w-full justify-center pb-6">
 		<span>{currentCard + 1} of {questionAnswers.length}</span>
 	</div>
-	<Flashcard q={qA.question} a={qA.answers} num={currentCard + 1} />
+	{#key questionAnswers}
+		<Flashcard q={qA.question} a={qA.answers} num={currentCard + 1} />
+	{/key}
 	<div class="w-[90%] md:w-[500px] flex justify-between pt-6 px-6 m-auto">
 		<div
 			class="border border-solid py-4 w-20 text-center rounded cursor-pointer"
 			on:click={previous}
 		>
 			Previous
+		</div>
+		<div
+			class="border border-solid py-4 w-20 text-center rounded cursor-pointer"
+			on:click={() => shuffleArray(questionAnswers)}
+		>
+			Shuffle
 		</div>
 		<div class="border border-solid py-4 w-20 text-center rounded cursor-pointer" on:click={next}>
 			Next
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	Flash Cards App
+<section class="w-full justify-center">
+	<h1 class="text-center font-extrabold text-xl">Flash Card Quizing App</h1>
+	<h2 class="text-center font-bold text-l">Add custom cards to quiz yourself.</h2>
 </section>
